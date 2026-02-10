22cc57a0a76c26f9f8cdfced0bc57ffc037a002b
dmdi-tech <d59692816@gmail.com>
2026-02-09 21:30:51 -0500

    adding more

--- src/reducers/todos.reducer.js
+++ src/reducers/todos.reducer.js
@@ -13,46 +13,94 @@ const actions = {
 
 const reducer = (state= initialState, action) => {
     switch(action.type) {
-        case actions.fetchTodos:
+        case actions.fetchTodos: {
+            return {
+                ...state,
+                isLoading: true,
+            };
+        }       
+        case actions.loadTodos: {
+            const todos = actions.records.map((record) =>{
+                const todo = {
+                    title: record.fields.title,
+                    id: record.id,
+                    isCompleted: record.fields.isCompleted ?? false,
+                };
+
+                if(!todo.isCompleted){
+                    todo.isCompleted = false;
+                }
+
+                return todo;
+            });
+
+            return {
+                ...state,
+                todoList: todos,
+                isLoading: false,
+            };
+        }
+        
+        case actions.setLoadError: {
+            return {
+                ...state,
+                errorMessage: action.error.message,
+                isLoading: false,
+            };
+        }
+
+        case actions.startRequest: {
+            return {
+                ...state,
+                isSaving: true,
+            };
+        }
+
+        case actions.addTodo: {
+            const savedTodo = { id:records[0].id, title: records[0].fields.title, isCompleted: true };
+            
+            if (!records[0].fields.isCompleted) {
+                savedTodo.isCompleted = false;
+            }
+
+            return {
+                ...state,
+                todoList: [...state.todoList, savedTodo],
+                isSaving: false,
+            };
+        }
+
+        case actions.endRequest: {
+            return {
+                ...state,
+                isLoading: false,
+                isSaving: false,
+            };
+        }
+
+        case actions.updateTodo: {
             return {
                 ...state,
             };
-        case actions.loadTodos:
+        }
+
+        case actions.completeTodo: {
             return {
                 ...state,
             };
-        case actions.setLoadError:
+        }
+
+        case actions.revertTodo: {
             return {
                 ...state,
             };
-        case actions.startRequest:
+        }
+
+        case actions.clearError: {
             return {
                 ...state,
             };
-        case actions.addTodo:
-            return {
-                ...state,
-            };
-        case actions.endRequest:
-            return {
-                ...state,
-            };
-        case actions.updateTodo:
-            return {
-                ...state,
-            };
-        case actions.completeTodo:
-            return {
-                ...state,
-            };
-        case actions.revertTodo:
-            return {
-                ...state,
-            };
-        case actions.clearError:
-            return {
-                ...state,
-            };
+        }
     }
 };
 
